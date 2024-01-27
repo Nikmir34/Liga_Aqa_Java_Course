c6b5bed7baba6e726aca2404294fe428b4cb5cdb
nmironov <nikita34mironov@yandex.ru>
2024-01-27 18:53:13 +0300

    Six lesson has been Finished

---
+++ src/main/code/one/TestClass.java
@@ -0,0 +1,4 @@
+package code.one;
+
+public class TestClass {
+}
---
+++ src/main/code/two/FirstProgram.java
@@ -0,0 +1,7 @@
+package code.two;
+
+public class FirstProgram {
+    public static void main(String[] args){
+        System.out.print("Текст для консоли");
+    }
+}
