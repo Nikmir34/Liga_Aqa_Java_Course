76ec98cc397782e8597c73a6edff5a8f1b40e54d
nmironov <nikita34mironov@yandex.ru>
2024-02-01 20:02:02 +0300

    Rework HW 5

--- src/test/java/AssertionHwTest.java
+++ src/test/java/AssertionHwTest.java
@@ -1,11 +1,10 @@
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import java.time.Duration;
-
 import java.time.Duration;
 
 @DisplayName(value = "Домашнее задание урок №5")
@@ -19,11 +18,7 @@ public class AssertionHwTest {
     @BeforeEach
     public void initDriver() {
         driver = new ChromeDriver();
-    }
-    @Test
-    @DisplayName(value = "Создание заметки, текста, выбор цвета, проверка заголовка")
-    public void titleTest(){
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         //Открытие стенда
         driver.get("http://172.24.120.5:8081/login");
         //Ввести значение в поле Логин
@@ -44,6 +39,13 @@ public class AssertionHwTest {
         driver.findElement(By.cssSelector(".PalettePopover_colorOption__lno8b:nth-of-type(4)")).click();
         //Кликаем на кнопку Ок
         driver.findElement(By.id("note-modal-save-btn-new_empty")).click();
+        driver.navigate().refresh();
+    }
+
+    @Test
+    @DisplayName(value = "Проверка заголовка")
+    public void checktitleTest(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //Получаем значение заголовка
         String titleText = driver.findElement(By.cssSelector(".Card_title__K3B5U")).getText();
         //Проверяем заголовок
@@ -52,18 +54,10 @@ public class AssertionHwTest {
 
     @Test
     @DisplayName(value = "Проверка текста в заметке")
-    public void textTest(){
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        //Открытие стенда
-        driver.get("http://172.24.120.5:8081/login");
-        //Ввести значение в поле Логин
-        driver.findElement(By.id("login-input")).sendKeys("nikmir");
-        //Ввести значение в поле Пароль
-        driver.findElement(By.id("password-input")).sendKeys("Qwerty$4xdd");
-        //Кликаем на нопку Войти
-        driver.findElement(By.cssSelector(".form_auth_button.btn.btn-primary:first-child")).click();
+    public void textTest() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //Получаем значение текста
-        String bodyText = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".Card_body__35O4X"))).getText();
+        String bodyText = driver.findElement(By.cssSelector(".Card_body__35O4X")).getText();
         //Проверяем текст
         Assertions.assertEquals("И тут тоже ничего нет ddx", bodyText, "Неверный текст в заметке");
     }
@@ -72,14 +66,6 @@ public class AssertionHwTest {
     @DisplayName(value = "Проверка цвета в заметке")
     public void colorTest(){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        //Открытие стенда
-        driver.get("http://172.24.120.5:8081/login");
-        //Ввести значение в поле Логин
-        driver.findElement(By.id("login-input")).sendKeys("nikmir");
-        //Ввести значение в поле Пароль
-        driver.findElement(By.id("password-input")).sendKeys("Qwerty$4xdd");
-        //Кликаем на нопку Войти
-        driver.findElement(By.cssSelector(".form_auth_button.btn.btn-primary:first-child")).click();
         //Получаем значение цвета
         String colorValue = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".Card_container__YjYSI"))).getCssValue("background-color");
         //Проверяем цвет
@@ -87,6 +73,8 @@ public class AssertionHwTest {
     }
     @AfterEach
     public void closeDriver() {
+        driver.findElement(By.cssSelector(".img-fluid:nth-of-type(2)")).click();
+        driver.findElement(By.cssSelector(".btn.btn-primary")).click();
         driver.quit();
     }
 
