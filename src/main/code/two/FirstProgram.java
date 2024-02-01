64f37550d85c634480ddc83b5cc70819f9e5399c
nmironov <nikita34mironov@yandex.ru>
2024-02-01 20:27:54 +0300

    Seven Lesson

--- src/main/code/two/FirstProgram.java
+++ src/main/code/two/FirstProgram.java
@@ -2,6 +2,21 @@ package code.two;
 
 public class FirstProgram {
     public static void main(String[] args){
-        System.out.print("Текст для консоли");
+        System.out.println("Текст для консоли");
+        hello();
+        welcome();
+        welcome();
+        text("Ross");
+    }
+    private static void hello(){
+        System.out.println("Hello");
+    }
+
+    private static void welcome(){
+        System.out.println("Welcome to my program");
+    }
+
+    private static void text(String name){
+        System.out.println("Name: "+name);
     }
 }
