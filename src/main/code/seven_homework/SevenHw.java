bebc966376c2d0e57ad015839d4005f1afd2ed4f
nmironov <nikita34mironov@yandex.ru>
2024-02-01 20:28:34 +0300

    The Seven Homework

---
+++ src/main/code/seven_homework/SevenHw.java
@@ -0,0 +1,18 @@
+package code.seven_homework;
+
+public class SevenHw {
+    public static void main(String[] args){
+        piconst();
+        value();
+
+    }
+    private static void piconst(){
+        final double PI = 3.14;
+        System.out.println("Значение числа π равно " + PI);
+    }
+
+    private static void value(){
+        int b = 19;
+        System.out.println("Искомое число равно " + b);
+    }
+}
