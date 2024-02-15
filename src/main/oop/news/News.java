e7f06fe31acb07cc8916588fb2f13ad8fe97a519
nmironov <nikita34mironov@yandex.ru>
2024-02-15 22:35:33 +0300

    Homework №12

---
+++ src/main/oop/news/News.java
@@ -0,0 +1,10 @@
+package oop.news;
+
+public class News {
+    protected static void getNews(){
+        System.out.println("Список последних новостей\n");
+        System.out.println("Save 15% on Priority e-bikes, Goal Zero 1500X power station now $1,260, and more\n");
+        System.out.println("Headlining today’s top deals is the limited-time Valentine’s sale that is still taking 15% off Priority Bicycles’ three e-bike models starting from $1,444. It is joined by a few discounts on Goal Zero power stations and bundles, led by the Yeti 1500X Portable…");
+        System.out.println("https://electrek.co/2024/02/15/priority-e-bikes-goal-zero-1500x-power-stations-more/");
+    }
+}
---
+++ src/main/oop/twelfth/twelfth_homework.java
@@ -0,0 +1,8 @@
+package oop.twelfth;
+import oop.news.News;
+
+public class twelfth_homework extends News {
+    public static void main(String[] Args){
+        getNews();
+    }
+}
