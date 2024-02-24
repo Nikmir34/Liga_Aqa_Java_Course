1d9db71e97c3e194137af3f78a408c552349f00f
nmironov <nikita34mironov@yandex.ru>
2024-02-24 20:27:19 +0300

    Homework №16 POF

---
+++ src/test/java/PageModalFactoryPages/AuthorizationPofPage.java
@@ -0,0 +1,41 @@
+package PageModalFactoryPages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AuthorizationPofPage {
+    private WebDriver driver;
+    private String urlPage = "http://172.24.120.5:8081/login";
+    @FindBy(id = "login-input")
+    private WebElement loginTextField;
+    @FindBy(id = "password-input")
+    private WebElement passwordTextField;
+    @FindBy(css = ".form_auth_button.btn.btn-primary:first-child")
+    private WebElement loginButton;
+
+
+    public AuthorizationPofPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goToAuthorizationPage() {
+        driver.get(urlPage);
+    }
+
+    public void fillInLogin(String loginText) {
+        loginTextField.sendKeys(loginText);
+    }
+
+    public void fillInPassword(String passwordText) {
+        passwordTextField.sendKeys(passwordText);
+        ;
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+}
---
+++ src/test/java/PageModalFactoryPages/MainPofPage.java
@@ -0,0 +1,30 @@
+package PageModalFactoryPages;
+
+import PageModalFactoryTests.PofTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MainPofPage extends PofTest {
+    private WebDriver driver;
+    @FindBy(id = "logout-btn")
+    private WebElement logoutButton;
+    @FindBy(id = "note-container-66667")
+    private WebElement note;
+
+
+    public MainPofPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public Boolean notesIsCreated() {
+        if (note.isDisplayed()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
