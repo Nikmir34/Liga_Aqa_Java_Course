f9b3dcd8a2315da44a2ff6557e19fac4a3dd8ffd
nmironov <nikita34mironov@yandex.ru>
2024-03-02 19:31:37 +0300

    Second Api HomeWork

---
+++ src/test/java/ApiSecondHw.java
@@ -0,0 +1,27 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName(value = "Домашнее задание №2 API")
+public class ApiSecondHw {
+
+    @DisplayName(value = "Вызов log")
+    @Test
+    public void LogTest(){
+        RestAssured.given()
+                .get("https://randomuser.me/api")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @DisplayName(value = "Вызов prettyprint и log")
+    @Test
+    public void PrettyLogTest(){
+        RestAssured.given()
+                .log().all()
+                .get("https://randomuser.me/api")
+                .andReturn()
+                .prettyPrint();
+    }
+}
